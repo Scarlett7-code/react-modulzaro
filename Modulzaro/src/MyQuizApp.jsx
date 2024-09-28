5f731b99e10561fda90c300a488b751b9be85697
Scarlett7-code <cs.zsuzsi333@gmail.com>
2024-09-28 18:02:48 +0200

    done

--- Modulzaro/src/MyQuizApp.jsx
+++ Modulzaro/src/MyQuizApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Questions from "./Questions";
 import QuizResult from "./QuizResult";
 
--- Modulzaro/src/Questions.jsx
+++ Modulzaro/src/Questions.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 function Questions({ questionsArray, setAnswer, id, questions }) {
 
